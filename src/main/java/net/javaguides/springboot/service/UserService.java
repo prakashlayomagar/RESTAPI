5a7141d993fbdb575ea6b9c8df7132a8a2b11790
prakash <petermagr4@gmail.com>
2026-01-12 00:54:39 -0500

    Updated userservice

--- src/main/java/net/javaguides/springboot/service/UserService.java
+++ src/main/java/net/javaguides/springboot/service/UserService.java
@@ -1,5 +1,40 @@
 package net.javaguides.springboot.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import net.javaguides.springboot.api.model.User;
+
+@Service
 public class UserService {
+
+    private List<User> userList;
+     
+    public UserService() {
+        userList = new ArrayList<>();
+        userList.add(new User(1, "John Doe",32,"john.doe@example.com"));
+        userList.add(new User(2, "Jane Smith",28,"jane.smith@example.com"));
+        userList.add(new User(3, "Bob Johnson",45,"bob.johnson@example.com"));
+        userList.add(new User(4, "Alice Brown",22,"alice.brown@example.com"));
+        
+        userList.addAll(Arrays.asList(user1,user2,user3,user4));
+    
+    }
+    
+    public Optional<User> getUser(Integer id){
+    	Optional <User> optional = Optional.empty();
+    	for(User user: userList) {
+    		if (id == user.getId()) {
+    			optional = Optional.of(user);
+    			return optional;
+    		}
+    	}
+		return optional;
+    	
+    }
 
 }
