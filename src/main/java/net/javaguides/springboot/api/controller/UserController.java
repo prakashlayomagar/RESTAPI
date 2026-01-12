8c2e6db6fc5bc307cc706545a0535a7cb7eb5273
prakash <petermagr4@gmail.com>
2026-01-12 00:39:05 -0500

    Updated controller

--- src/main/java/net/javaguides/springboot/api/controller/UserController.java
+++ src/main/java/net/javaguides/springboot/api/controller/UserController.java
@@ -1,4 +1,22 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+import net.javaguides.springboot.service.UserService;
+
+@RestController 
 public class UserController {
+	
+	private final UserService userService;
+	
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService ;
+	}
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id) {
+       Optional<User> user = userService.getUserById(id);
+       return user.orElse(null);
+    }
 
 }
